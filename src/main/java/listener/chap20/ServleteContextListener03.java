32d1570dd11f6ba79a7633bdef2a7d875b21fd06
sikdong <zmstnv1119@naver.com>
2022-10-11 17:44:54 +0900

    1011

---
+++ src/main/java/listener/chap20/ServleteContextListener03.java
@@ -0,0 +1,41 @@
+package listener.chap20;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ServleteContextListener03
+ *
+ */
+/* @WebListener */
+public class ServleteContextListener03 implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ServleteContextListener03() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  {
+    	ServletContext application = sce.getServletContext();
+    	String param1 = application.getInitParameter("init-param1");
+    	System.out.println(param1);
+    	
+    	String param2 = application.getInitParameter("init-param2");
+    	System.out.println(param2);
+    }
+	
+}
--- src/main/webapp/WEB-INF/view/chap17/view07.jsp
+++ src/main/webapp/WEB-INF/view/chap17/view07.jsp
@@ -10,13 +10,28 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-	<h3>06jsp</h3>
-	<form action="view07.jsp" method="post">
-		이름 : <input type="text" name="name">
-		성별 : <input type="text" name="gender">
-		<input type="submit" value="전송"> 
-	</form>
+	<h1>검색 결과</h1>
+	<h1>책제목 : ${book.title }</h1>
+	<h1>가격 : ${book.price }</h1>
 
+	<hr>
+	<h3>${sessionScope.message }</h3>
+	<c:remove var="message" scope="session"/>
+	
+	<form action="" method="post">
+		제목 : <input type="text" name="title"> <br>
+		가격 : <input type="number" name="price" > <br>
+		<input type="submit" value="등록">
+	</form>
+	
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
+
+
+
+
+
+
